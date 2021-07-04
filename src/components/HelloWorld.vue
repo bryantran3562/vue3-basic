451c38321b44084e72ed5102287f7a20a3916832
bryantran3562 <bryantran3562@gmail.com>
2021-07-04 16:41:12 -0500

    Adding template ref

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,8 @@
 <template>
 
   <h2>This is child component</h2>
+  <input type="text" class="text" ref="name">
+  <button @click="handleClick"></button>
 
 
 </template>
@@ -8,6 +10,16 @@
 <script>
 export default {
   name: 'HelloWorld',
+  methods: {
+    handleClick(){
+      //BT- Template ref is nothing more that - whenever you need to grap an HTML element, you can just give it
+      //    ref="any_name_you_want". Then you can get that hanle by the method below.
+      //Notes: classList is property of the javascript, it has nothing to do with Vue.
+      //       add is a method of the java script. Then we add an 'active' class to this HTML element.
+      
+      this.$refs.name.classList.add('active')
+    }
+  }
 
 }
 </script>
